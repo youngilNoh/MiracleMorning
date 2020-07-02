cddd3173731e1c41fe466a15a54bd2668331b02b
yesimnoh <yesimnoh@gmail.com>
2020-07-02 22:56:16 +0000

    Finish extract jobs from indeed

--- indeed.py
+++ indeed.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 LIMIT = 40
 URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
-def extract_indeed_pages():
+def get_last_page():
   result = requests.get(URL)
   soup = BeautifulSoup(result.text, "html.parser")
 
@@ -18,20 +18,39 @@ def extract_indeed_pages():
   max_page = pages[-1]
   return max_page
 
-def extract_indeed_jobs(last_page):
-  jobs = []
-  # for page in range(last_page):
-  result = requests.get(f"{URL}&start={0*LIMIT}")
-  soup = BeautifulSoup(result.text, "html.parser")
-  results = soup.find_all("div",{"class":"jobsearch-SerpJobCard"})
-  for result in results:
-    title = result.find("h2",{"class":"title"}).find("a")["title"]
-    company = result.find("span",{"class":"company"})
+def extract_job(html):
+  job_id = html["data-jk"]
+  title = html.find("h2",{"class":"title"}).find("a")["title"]
+  company = html.find("span",{"class":"company"})
+  if company is not None:
     company_anchor = company.find("a")
     if company_anchor is not None:
       company = str(company_anchor.string)
     else:
       company = str(company.string)
-    company = company.strip()
-    print(title,"///",company)
+  else:
+    company = str("Remote")
+  company = company.strip()
+  location = html.find("div",{"class":"recJobLoc"})["data-rc-loc"]
+  return {
+    'title':title,
+    'company':company,
+    'location':location,
+    'link':f"https://www.indeed.com/viewjob?jk={job_id}"}
+
+def extract_jobs(last_page):
+  jobs = []
+  for page in range(last_page):
+    print(f"Scrapping page {page}")
+    result = requests.get(f"{URL}&start={page*LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div",{"class":"jobsearch-SerpJobCard"})
+    for result in results:
+      job = extract_job(result)
+      jobs.append(job)
+  return jobs
+
+def get_jobs():
+  last_pages = get_last_page()
+  jobs = extract_jobs(last_pages)
   return jobs
