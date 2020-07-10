ec5126f6662e63419d19d798c089af063b547736
youngilNoh <yesimnoh@gmail.com>
2020-07-11 08:22:45 +0900

    Clear folder

--- Python/WebScrapper/main.py
+++ Python/WebScrapper/main.py
@@ -1,3 +1,5 @@
+from flask import Flask, render_template, request
+
 class Car():
 
   def __init__(self, *args, **kwargs):
@@ -27,3 +29,27 @@ porche = Convertible(color="green", price="$40")
 print(porche)
 print(porche.take_off())
 print(porche.color)
+
+
+app = Flask("SuperScrapper")
+
+@app.route("/")
+def home():
+  return "Hello! welcome to mi casa!"
+
+@app.route("/<username>")
+def potato(username):
+  return f"Hello {username} how are you doing"
+
+@app.route("/html")
+def html():
+  # html template load
+  return render_template("flasktest.html")
+
+@app.route("/report")
+def report():
+  word = request.args.get('word')
+  return render_template("report.html", word=word)
+
+
+app.run(host="0.0.0.0")
