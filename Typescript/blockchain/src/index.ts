bdc8af69f161c60576684da64d8e5fdf24190ee1
yesimnoh <yesimnoh@gmail.com>
2020-09-25 16:42:27 +0900

    add git ignore and typescript_blockchain

---
+++ Typescript/blockchain/src/index.ts
@@ -0,0 +1,22 @@
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+
+const genesisBlock:Block = new Block(0,"2020202020","","hello",123456);
+
+let blockchain:[Block] = [genesisBlock];
+
+console.log(blockchain);
+
+export {};
