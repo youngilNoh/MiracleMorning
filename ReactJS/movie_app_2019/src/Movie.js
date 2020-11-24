3d7439a79f49b823be815f1047c4f8b261eae8e6
yesimnoh <yesimnoh@gmail.com>
2020-11-24 22:02:46 +0900

    commit movie

--- ReactJS/movie_app_2019/src/Movie.js
+++ ReactJS/movie_app_2019/src/Movie.js
@@ -1,27 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movies({ id, year, title, summary, poster, genres }) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <div className="movies__movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
+        <ul className="movie__genres">
           {genres.map((genre, index) => (
-            <li className="genres__genre" key={index}>
+            <li key={index} className="genres__genre">
               {genre}
             </li>
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
 }
 
-Movies.propTypes = {
+Movie.propTypes = {
   id: PropTypes.number.isRequired,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
@@ -30,4 +31,4 @@ Movies.propTypes = {
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
-export default Movies;
+export default Movie;
